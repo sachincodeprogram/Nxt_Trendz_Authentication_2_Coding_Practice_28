7348a56162ef8a7c0bdcaaeddd4ffaff470a9dcd
sachincodeprogram <132731221+sachincodeprogram@users.noreply.github.com>
2023-05-28 17:51:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,16 +5,15 @@ import Home from './components/Home'
 import Products from './components/Products'
 import Cart from './components/Cart'
 import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/products" component={Products} />
-    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/cart" component={cart} />
     <Route component={NotFound} />
   </Switch>
 )
